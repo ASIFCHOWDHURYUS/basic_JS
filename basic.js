137dc8f86ede7be237f160ee72e1f4a990cb55e4
ZOOMEX CORPORATION <asifchowus@gmail.com>
2023-01-12 12:50:49 +0600

    condition addae

---
+++ basic.js
@@ -0,0 +1,45 @@
+let greetting = "Hello my name is";
+let myname = "Asif";
+myname += " " + "Chowdhury";
+
+
+let str = "And I have";
+let apples = 30;
+apples *= 7;
+let end = "Apples";
+
+console.log(greetting, myname, str, apples, end);
+
+let a = 4 + 7;
+console.log(a);
+let b = 5 * 9;
+console.log(b);
+
+let c = b / a;
+console.log(c)
+
+let x = 32;
+if (x == 9) {
+  consele.log('true');
+} else {
+  console.log('false');
+}
+
+
+let y = 'green';
+if (y == "blue") {
+  console.log("blue is detected")
+} else if (y == "green") {
+  console.log("green is detected")
+} else {
+  console.log("not blue not green")
+}
+
+
+
+let n = 50;
+if (n > 45) {
+  if (n < 65) {
+    console.log(n);
+  }
+}
